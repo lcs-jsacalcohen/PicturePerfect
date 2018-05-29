021e25e612880e8e982b65795c7b8e3b2770f4f3
Russell Gordon <jsacalcohen@lcs.on.ca>
2018-05-28 20:20:22 -0400

    done

--- PicturePerfect/main.swift
+++ PicturePerfect/main.swift
@@ -2,39 +2,62 @@
 //  main.swift
 //  PicturePerfect
 //
-
 import Foundation
 
 // INPUT
 // Global variable to use later in program
-var countOfPhotoArrangementsToBeConsidered = 3
-
 // Write a loop to actually collect the number of photo arrangements to be considered
 // e.g.: write the rest of the INPUT section
-
+var givenPhotos = 0
+while 1==1 {
+    print("How many photo arrangements will be considered?")
+    guard let input = readLine() else {
+        continue
+    }
+    guard let intInput = Int(input) else {
+        continue
+    }
+    if intInput < 1 || intInput > 10 {
+        continue
+    }
+    givenPhotos = intInput
+    break
+}
 
 // PROCESS & OUTPUT
-// Implement the primary logic of the problem here
-// Some output may be given here if you desire
-
 // Collect the number of pictures in each arrangement using this loop
-for counter in 1...countOfPhotoArrangementsToBeConsidered {
+for counter in 1...givenPhotos {
     
     // Ask how many pictures in this particular arrangement
     print("How many pictures in photo arrangement #\(counter)?")
     
-    // Get the input (use guard-let to guarantee it is not nil)
-    // and then print it out
     guard let givenInput = readLine() else {
-        // If someone enters nil input, just skip to the next line
+        continue
+    }
+    guard let numberOfPhotos = Int(givenInput) else {
         continue
     }
     
-    // What was provided?
-    print("The given input was: \(givenInput)")
+    if numberOfPhotos > 65_000 || numberOfPhotos < 0 {
+        continue
+    }
     
-    // Implement the rest of your logic here...
-
-
+    var oneSide = 0
+    var arrayOfSides : [Int] = []
+    for i in 1...numberOfPhotos {
+        if numberOfPhotos % i == 0 {
+            arrayOfSides.append(i)
+        }
+    }
+    
+    oneSide = arrayOfSides[(arrayOfSides.count - 1) / 2]
+    
+    
+    // What was provided?
+    let secondSide = numberOfPhotos / oneSide
+    let perimeter = 2 * secondSide + 2 * oneSide
+    print("Minimum perimeter is \(perimeter) with dimensions \(secondSide) and \(oneSide)")
+    
+    
+    
 }
-
